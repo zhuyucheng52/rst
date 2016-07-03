507add804083462b9fdc88772fd0470cd203a2d1
Echo <zhuyucheng52@outlook.com>
2016-07-03 22:07:43 +0800

    add oper log async

--- src/main/java/com/echo/rst/operlog/OperLogRepository.java
+++ src/main/java/com/echo/rst/operlog/OperLogRepository.java
@@ -1,6 +1,7 @@
 package com.echo.rst.operlog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
 
@@ -8,6 +9,8 @@ import java.util.List;
  * Created by echo on 16-6-26.
  */
 public interface OperLogRepository extends JpaRepository<OperLog, Long> {
+    @Async
+    OperLog save(OperLog log);
     List<OperLog> findByState(Integer state);
 }
 
